8d547abc0ad7c0c85d5a2b2b4c7a7a6c24dd307f
Yurii Syroied <syroiedyur@gmail.com>
2026-09-26 00:41:56 +0300

    solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -108,8 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "taxi.Driver"
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
 
 LANGUAGE_CODE = "en-us"
 
